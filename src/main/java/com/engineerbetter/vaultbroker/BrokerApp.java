9a32c917beba6647bae6972aadf7c712206d351d
Dan Young <dan.young@engineerbetter.com>
2016-06-08 14:17:47 +0100

    Create as standalone Spring Boot Application

---
+++ src/main/java/com/engineerbetter/vaultbroker/BrokerApp.java
@@ -0,0 +1,14 @@
+package com.engineerbetter.vaultbroker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class BrokerApp {
+	
+	public static void main() {
+		SpringApplication.run(BrokerApp.class);
+	}
+
+}
---
+++ src/main/java/com/engineerbetter/vaultbroker/BrokerResponse.java
@@ -0,0 +1,10 @@
+package com.engineerbetter.vaultbroker;
+
+public class BrokerResponse {
+	
+	   public String getResponse() 
+	   {
+		    return "Hello!";
+	   }
+
+}
