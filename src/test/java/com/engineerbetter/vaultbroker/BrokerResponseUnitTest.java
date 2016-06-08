ae968e758db5c572db1f22f72b31eea116698eee
Dan Young <dan.young@engineerbetter.com>
2016-06-08 14:37:11 +0100

    removed unused instance of RestTemplate from unit test

--- src/test/java/com/engineerbetter/vaultbroker/BrokerResponseUnitTest.java
+++ src/test/java/com/engineerbetter/vaultbroker/BrokerResponseUnitTest.java
@@ -3,12 +3,8 @@ package com.engineerbetter.vaultbroker;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
-import org.springframework.boot.test.TestRestTemplate;
-import org.springframework.web.client.RestTemplate;
 
 public class BrokerResponseUnitTest {
-	
-	   private RestTemplate rest = new TestRestTemplate();
 	
 	   private BrokerResponse Broker;
 		
